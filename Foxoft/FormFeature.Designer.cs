f7d7353d89e3c9ab2ff95c8d8c90a13b60c1460d
Subhan <huseynzadesubhan@outlook.com>
2022-11-27 09:17:06 +0400

    das dasd a

--- Foxoft/FormFeature.Designer.cs
+++ Foxoft/FormFeature.Designer.cs
@@ -29,19 +29,78 @@ namespace Foxoft
       /// </summary>
       private void InitializeComponent()
       {
+         this.layoutControl1 = new DevExpress.XtraLayout.LayoutControl();
+         this.buttonEdit1 = new DevExpress.XtraEditors.ButtonEdit();
+         this.Root = new DevExpress.XtraLayout.LayoutControlGroup();
+         this.layoutControlItem1 = new DevExpress.XtraLayout.LayoutControlItem();
+         ((System.ComponentModel.ISupportInitialize)(this.layoutControl1)).BeginInit();
+         this.layoutControl1.SuspendLayout();
+         ((System.ComponentModel.ISupportInitialize)(this.buttonEdit1.Properties)).BeginInit();
+         ((System.ComponentModel.ISupportInitialize)(this.Root)).BeginInit();
+         ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem1)).BeginInit();
          this.SuspendLayout();
+         // 
+         // layoutControl1
+         // 
+         this.layoutControl1.Controls.Add(this.buttonEdit1);
+         this.layoutControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+         this.layoutControl1.Location = new System.Drawing.Point(0, 0);
+         this.layoutControl1.Name = "layoutControl1";
+         this.layoutControl1.Root = this.Root;
+         this.layoutControl1.Size = new System.Drawing.Size(298, 268);
+         this.layoutControl1.TabIndex = 0;
+         this.layoutControl1.Text = "layoutControl1";
+         // 
+         // buttonEdit1
+         // 
+         this.buttonEdit1.Location = new System.Drawing.Point(117, 12);
+         this.buttonEdit1.Name = "buttonEdit1";
+         this.buttonEdit1.Properties.Buttons.AddRange(new DevExpress.XtraEditors.Controls.EditorButton[] {
+            new DevExpress.XtraEditors.Controls.EditorButton()});
+         this.buttonEdit1.Size = new System.Drawing.Size(169, 20);
+         this.buttonEdit1.StyleController = this.layoutControl1;
+         this.buttonEdit1.TabIndex = 4;
+         // 
+         // Root
+         // 
+         this.Root.EnableIndentsWithoutBorders = DevExpress.Utils.DefaultBoolean.True;
+         this.Root.GroupBordersVisible = false;
+         this.Root.Items.AddRange(new DevExpress.XtraLayout.BaseLayoutItem[] {
+            this.layoutControlItem1});
+         this.Root.Name = "Root";
+         this.Root.Size = new System.Drawing.Size(298, 268);
+         this.Root.TextVisible = false;
+         // 
+         // layoutControlItem1
+         // 
+         this.layoutControlItem1.Control = this.buttonEdit1;
+         this.layoutControlItem1.Location = new System.Drawing.Point(0, 0);
+         this.layoutControlItem1.Name = "layoutControlItem1";
+         this.layoutControlItem1.Size = new System.Drawing.Size(278, 248);
+         this.layoutControlItem1.TextSize = new System.Drawing.Size(93, 13);
          // 
          // FormFeature
          // 
          this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
          this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-         this.ClientSize = new System.Drawing.Size(358, 241);
+         this.ClientSize = new System.Drawing.Size(298, 268);
+         this.Controls.Add(this.layoutControl1);
          this.Name = "FormFeature";
-         this.Text = "XtraForm1";
+         this.Text = "FormFeature";
+         ((System.ComponentModel.ISupportInitialize)(this.layoutControl1)).EndInit();
+         this.layoutControl1.ResumeLayout(false);
+         ((System.ComponentModel.ISupportInitialize)(this.buttonEdit1.Properties)).EndInit();
+         ((System.ComponentModel.ISupportInitialize)(this.Root)).EndInit();
+         ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem1)).EndInit();
          this.ResumeLayout(false);
 
       }
 
       #endregion
+
+      private DevExpress.XtraLayout.LayoutControl layoutControl1;
+      private DevExpress.XtraEditors.ButtonEdit buttonEdit1;
+      private DevExpress.XtraLayout.LayoutControlGroup Root;
+      private DevExpress.XtraLayout.LayoutControlItem layoutControlItem1;
    }
 }
--- Foxoft/FormFeature.cs
+++ Foxoft/FormFeature.cs
@@ -1,8 +1,17 @@
 ﻿using DevExpress.XtraEditors;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace Foxoft
 {
-   public partial class FormFeature : XtraForm
+   public partial class FormFeature : DevExpress.XtraEditors.XtraForm
    {
       public FormFeature()
       {
