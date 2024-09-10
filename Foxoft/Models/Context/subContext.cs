e733936b892634f2ae534c36dd94e7a344119164
Subhan <huseynzadesubhan@outlook.com>
2024-09-10 16:14:47 +0400

    stabil version

--- Foxoft/Models/Context/subContext.cs
+++ Foxoft/Models/Context/subContext.cs
@@ -490,9 +490,10 @@ namespace Foxoft.Models
 
             modelBuilder.Entity<DcPaymentMethod>().HasData(
                 new DcPaymentMethod { PaymentMethodId = 1, PaymentTypeCode = 1, PaymentMethodDesc = "Nağd" },
-                new DcPaymentMethod { PaymentMethodId = 2, PaymentTypeCode = 1, PaymentMethodDesc = "Çatdırılma zamanı nağd ödə" },
-                new DcPaymentMethod { PaymentMethodId = 3, PaymentTypeCode = 2, PaymentMethodDesc = "Saytda nağd ödə" },
-                new DcPaymentMethod { PaymentMethodId = 4, PaymentTypeCode = 2, PaymentMethodDesc = "Bir Kart" }
+                new DcPaymentMethod { PaymentMethodId = 2, PaymentTypeCode = 2, PaymentMethodDesc = "Daxili Kredit" },// Internal Credit
+                new DcPaymentMethod { PaymentMethodId = 3, PaymentTypeCode = 2, PaymentMethodDesc = "Bir Kart" },
+                new DcPaymentMethod { PaymentMethodId = 4, PaymentTypeCode = 1, PaymentMethodDesc = "Çatdırılma zamanı nağd ödə" },
+                new DcPaymentMethod { PaymentMethodId = 5, PaymentTypeCode = 2, PaymentMethodDesc = "Saytda nağd ödə" }
                 );
 
             modelBuilder.Entity<DcProcess>().HasData(
