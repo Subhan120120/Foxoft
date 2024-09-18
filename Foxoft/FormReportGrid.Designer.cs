02065efd45a6319acf0278dc3884f534623e5a9b
Subhan <huseynzadesubhan@outlook.com>
2024-09-18 17:14:49 +0400

    productGallery bitdi

--- Foxoft/FormReportGrid.Designer.cs
+++ Foxoft/FormReportGrid.Designer.cs
@@ -32,7 +32,6 @@ namespace Foxoft
             components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormReportGrid));
             gC_Report = new MyGridControl();
-            gV_Report = new MyGridView(this);
             ribbonControl1 = new DevExpress.XtraBars.Ribbon.RibbonControl();
             bBI_LayoutSave = new DevExpress.XtraBars.BarButtonItem();
             bBI_LayoutLoad = new DevExpress.XtraBars.BarButtonItem();
