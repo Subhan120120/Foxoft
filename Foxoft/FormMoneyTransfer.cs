a058170b438087807b0098e852a102888bd3ff1f
Subhan <Subhan@DESKTOP-HQDR1G1>
2026-01-31 23:53:33 +0400

    fx

--- Foxoft/FormMoneyTransfer.cs
+++ Foxoft/FormMoneyTransfer.cs
@@ -278,7 +278,7 @@ namespace Foxoft
         {
             gV_PaymentLine.SetRowCellValue(e.RowHandle, colPaymentHeaderId, trPaymentHeader.PaymentHeaderId);
             gV_PaymentLine.SetRowCellValue(e.RowHandle, colPaymentLineId, Guid.NewGuid());
-            gV_PaymentLine.SetRowCellValue(e.RowHandle, colPaymentTypeCode, 1);
+            gV_PaymentLine.SetRowCellValue(e.RowHandle, colPaymentTypeCode, PaymentType.Cash);
             gV_PaymentLine.SetRowCellValue(e.RowHandle, colPaymentMethodId, 1);
 
             string cashReg = trPaymentHeader.ToCashRegCode;
