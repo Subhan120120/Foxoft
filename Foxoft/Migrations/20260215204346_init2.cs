789e1957f0fc6aa7821cb4e54b1dafc3a42415b7
Subhan <Subhan@DESKTOP-HQDR1G1>
2026-02-16 00:44:30 +0400

    gh

---
+++ Foxoft/Migrations/20260215204346_init2.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace Foxoft.Migrations
+{
+    /// <inheritdoc />
+    public partial class init2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "DcClaims",
+                columns: new[] { "ClaimCode", "CategoryId", "ClaimDesc", "ClaimTypeId" },
+                values: new object[,]
+                {
+                    { "PosAllowPaymentDifferenceOfInvoice", 2, "Faktura ilə ödəniş arasında fərqə icazə", (byte)1 },
+                    { "PosNewInvoice", 2, "POS Yeni Faktura", (byte)1 }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "DcClaims",
+                keyColumn: "ClaimCode",
+                keyValue: "PosAllowPaymentDifferenceOfInvoice");
+
+            migrationBuilder.DeleteData(
+                table: "DcClaims",
+                keyColumn: "ClaimCode",
+                keyValue: "PosNewInvoice");
+        }
+    }
+}
