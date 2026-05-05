c63ff55b1314b759116ee11e97ad4ee580aed4f2
Subhan <Subhan@DESKTOP-O34PGPM>
2026-05-05 14:42:44 +0400

    af

--- Foxoft/AppCode/Report/Report_Embedded_PaymentReport.sql
+++ Foxoft/AppCode/Report/Report_Embedded_PaymentReport.sql
@@ -15,13 +15,19 @@ select ph.*
 	, cari.PhoneNum
 	, CashRegisterDesc = kassa.CurrAccDesc
 	, DcPaymentTypes.PaymentTypeDesc
-	, CurrAccBalance = dbo.CurrAccBalance(ph.CurrAccCode, DocumentDate)
+	, CurrAccBalance = dbo.CurrAccBalance(ph.CurrAccCode, CAST(ph.DocumentDate as Datetime) + CAST(ph.DocumentTime as Datetime))
+	
+	, StorePhoneNum = store.PhoneNum
+	, StoreAddress = store.Address
+
 	from TrPaymentLines pl 
 	left join TrPaymentHeaders ph on pl.PaymentHeaderId = ph.PaymentHeaderId
 	left join DcPaymentTypes on DcPaymentTypes.PaymentTypeCode = pl.PaymentTypeCode
 	left join DcCurrAccs cari on cari.CurrAccCode = ph.CurrAccCode
 	left join DcCurrAccs kassa on kassa.CurrAccCode = pl.CashRegisterCode
 	left join DcProcesses on DcProcesses.ProcessCode = ph.ProcessCode
+	left join DcCurrAccs store on store.CurrAccCode = ph.StoreCode
+	left join DcCurrencies on DcCurrencies.CurrencyCode = pl.CurrencyCode
 
 	where ph.ProcessCode = 'PA' AND ph.PaymentHeaderId = @PaymentHeaderId
 	order by DocumentDate
