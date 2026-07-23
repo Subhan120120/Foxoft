61144119d5345887119963b125e1ed6ba789498e
Subhan <Subhan@DESKTOP-O34PGPM>
2026-07-23 20:50:55 +0400

    FUNCTION GetProductCost fix

--- Foxoft/Migrations/Functions/GetProductCost.cs
+++ Foxoft/Migrations/Functions/GetProductCost.cs
@@ -11,32 +11,107 @@ namespace Foxoft.Migrations
         protected override void Up(MigrationBuilder migrationBuilder)
         {
             migrationBuilder.Sql(@"
-                                CREATE OR ALTER FUNCTION [dbo].[GetProductCost] (
-                                    @productCode VARCHAR(50),    -- Assuming productCode is a string with a maximum length
-                                    @documentDatetime DATETIME = NULL  -- Optional parameter, default value is NULL
+                CREATE OR ALTER FUNCTION [dbo].[GetProductCost]
+                (
+                    @ProductCode NVARCHAR(30),
+                    @DocumentDatetime DATETIME = NULL
+                )
+                RETURNS DECIMAL(18, 2)
+                AS
+                BEGIN
+                    DECLARE @Result DECIMAL(18, 2);
+                    DECLARE @FilterDate DATE;
+                    DECLARE @FilterTime TIME;
+                
+                    SET @FilterDate = CONVERT(DATE, @DocumentDatetime);
+                    SET @FilterTime = CONVERT(TIME, @DocumentDatetime);
+                
+                    IF @DocumentDatetime IS NULL
+                    BEGIN
+                        SELECT
+                            @Result = CONVERT
+                            (
+                                DECIMAL(18, 2),
+                                AVG
+                                (
+                                    CONVERT
+                                    (
+                                        DECIMAL(38, 6),
+                                        ISNULL(ilE.PriceDiscountedLoc, 0)
+                                        + ISNULL(ilE.LineExpences, 0)
+                                    )
                                 )
-                                RETURNS DECIMAL(18, 2)
-                                AS
-                                BEGIN
-                                    DECLARE @result DECIMAL(18, 2);
-                                
-                                    SELECT @result = CAST(
-                                        (
-                                            SELECT TOP 1 AVG(ISNULL(PriceDiscountedLoc, 0)) + SUM(ISNULL(LineExpences, 0))
-                                            FROM TrInvoiceLines il
-                                            JOIN TrInvoiceHeaders ih ON ih.InvoiceHeaderId = il.InvoiceHeaderId
-                                            LEFT JOIN trInvoiceLineExts ilE ON ilE.InvoiceLineId = il.InvoiceLineId
-                                            WHERE ih.ProcessCode IN ('RP', 'CI')
-                                                AND ih.IsReturn = 0
-                                                AND il.ProductCode = @productCode
-                                                AND (@documentDatetime IS NULL OR (CAST(ih.DocumentDate AS DATETIME) + CAST(ih.DocumentTime AS DATETIME) < @documentDatetime))
-                                            GROUP BY ih.ProcessCode, ih.IsReturn, ProductCode, DocumentDate
-                                            ORDER BY DocumentDate DESC
-                                        ) AS DECIMAL(18, 2)
-                                    );
-                                
-                                    RETURN @result;
-                                END;
+                            )
+                        FROM
+                        (
+                            SELECT TOP (1)
+                                InvoiceHeaderId = il.InvoiceHeaderId
+                            FROM dbo.TrInvoiceLines il
+                            INNER JOIN dbo.TrInvoiceHeaders ih
+                                ON ih.InvoiceHeaderId = il.InvoiceHeaderId
+                            WHERE il.ProductCode = @ProductCode
+                              AND ih.ProcessCode IN ('RP', 'CI')
+                              AND ih.IsReturn = 0
+                            ORDER BY
+                                ih.DocumentDate DESC,
+                                ih.DocumentTime DESC,
+                                ih.InvoiceHeaderId DESC
+                        ) LatestPurchase
+                        INNER JOIN dbo.TrInvoiceLines il
+                            ON il.InvoiceHeaderId = LatestPurchase.InvoiceHeaderId
+                           AND il.ProductCode = @ProductCode
+                        LEFT JOIN dbo.TrInvoiceLineExts ilE
+                            ON ilE.InvoiceLineId = il.InvoiceLineId;
+                    END
+                    ELSE
+                    BEGIN
+                        SELECT
+                            @Result = CONVERT
+                            (
+                                DECIMAL(18, 2),
+                                AVG
+                                (
+                                    CONVERT
+                                    (
+                                        DECIMAL(38, 6),
+                                        ISNULL(ilE.PriceDiscountedLoc, 0)
+                                        + ISNULL(ilE.LineExpences, 0)
+                                    )
+                                )
+                            )
+                        FROM
+                        (
+                            SELECT TOP (1)
+                                InvoiceHeaderId = il.InvoiceHeaderId
+                            FROM dbo.TrInvoiceLines il
+                            INNER JOIN dbo.TrInvoiceHeaders ih
+                                ON ih.InvoiceHeaderId = il.InvoiceHeaderId
+                            WHERE il.ProductCode = @ProductCode
+                              AND ih.ProcessCode IN ('RP', 'CI')
+                              AND ih.IsReturn = 0
+                              AND
+                              (
+                                  ih.DocumentDate < @FilterDate
+                                  OR
+                                  (
+                                      ih.DocumentDate = @FilterDate
+                                      AND ih.DocumentTime < @FilterTime
+                                  )
+                              )
+                            ORDER BY
+                                ih.DocumentDate DESC,
+                                ih.DocumentTime DESC,
+                                ih.InvoiceHeaderId DESC
+                        ) LatestPurchase
+                        INNER JOIN dbo.TrInvoiceLines il
+                            ON il.InvoiceHeaderId = LatestPurchase.InvoiceHeaderId
+                           AND il.ProductCode = @ProductCode
+                        LEFT JOIN dbo.TrInvoiceLineExts ilE
+                            ON ilE.InvoiceLineId = il.InvoiceLineId;
+                    END;
+                
+                    RETURN @Result;
+                END;
 
                                     ");
         }
